0d287d48841b0e69c8dda81b4535bd5f1cd2917d
revvlover218 <reshalluchman@yahoo.com>
2016-05-19 00:29:43 +0200

    added s boxes

--- DES Cipher/DEScipher.cpp
+++ DES Cipher/DEScipher.cpp
@@ -297,6 +297,47 @@ void DES::encrypt()
 		24, 25, 26, 27, 28, 29,
 		28, 29, 30, 31, 32, 1 };
 
+	//S- Boxes
+	int sbox1[4][16] = { { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 },
+	{ 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 },
+	{ 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0 },
+	{ 15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 } };
+
+	int sbox2[4][16] = { { 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10 },
+	{ 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5 },
+	{ 0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15 },
+	{ 13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 } };
+
+	int sbox3[4][16] = { { 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8 },
+	{ 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1 },
+	{ 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7 },
+	{ 1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 } };
+
+	int sbox4[4][16] = { { 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15 },
+	{ 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9 },
+	{ 10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4 },
+	{ 3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 } };
+
+	int sbox5[4][16] = { { 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9 },
+	{ 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6 },
+	{ 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14 },
+	{ 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 } };
+
+	int sbox6[4][16] = { { 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11 },
+	{ 10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8 },
+	{ 9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6 },
+	{ 4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 } };
+
+	int sbox7[4][16] = { { 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1 },
+	{ 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6 },
+	{ 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2 },
+	{ 6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 } };
+
+	int sbox8[4][16] = { { 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7 },
+	{ 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 },
+	{ 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8 },
+	{ 2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 } };
+
 	system("cls");
 
 	//Run a loop for each block
@@ -347,7 +388,7 @@ void DES::encrypt()
 			cout << "\nTaking expanded R and XOR with key: " << round + 1 << endl;
 			for (int i = 0; i < 48; i++)
 			{
-				R_Expand[i] = R_Expand[i] ^ enc_key[round][i];
+				R_Expand[i] = R_Expand[i] ^ enc_key[round][i];		//'^' used to implement XOR operation 
 			}
 
 			cout << "\nGrouping 48- bits into 8 groups of 6- bits so that S- box can be applied." << endl;
@@ -363,8 +404,31 @@ void DES::encrypt()
 				}
 			}
 
+			cout << "\nApplying S- Box to each group." << endl;
+			int n = 0;
+			int num = 0; //Stores the result of the S-box
 			
+			for (int i = 0; i < 8; i++)
+			{
 
+				//Calculate Row to access in S- Box. To get the row number the first and last bit in each group is
+				//converted to decimal.
+				int row = (R_group[i][0] * 2) + (R_group[i][5]);
+				//Calculate Column to access in S- Box. To get the column number the middle 4- bits in each group is
+				//converted to decimal.
+				int column = ((R_group[i][1] * 8) + (R_group[i][2] * 4)	+ (R_group[i][3] * 2) + (R_group[i][4]));
+
+				if (i == 0) { num = sbox1[row][column]; }
+				if (i == 1) { num = sbox2[row][column]; }
+				if (i == 2) { num = sbox3[row][column]; }
+				if (i == 3) { num = sbox4[row][column]; }
+				if (i == 4) { num = sbox5[row][column]; }
+				if (i == 5) { num = sbox6[row][column]; }
+				if (i == 6) { num = sbox7[row][column]; }
+				if (i == 7) { num = sbox8[row][column]; }
+
+
+			}
 
 
 
